f3fafa79b934fdd46675f2d05758299a585849fb
Henry Ofodile <ofodilechukwuka@gmail.com>
2019-04-25 22:55:11 +0100

    create generatemessage utils

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,24 @@
 var socket = io();
+// var socket = io.connect('http://localhost:3000');
 socket.on("connect", function(){
   console.log("Connected to the server!");
-  socket.emit("createMessage",{
-    from:"Henry",
-    text:"Yes, works absolutely!"
-  });
+  // socket.emit("createMessage",{
+  //   from:"Henry",
+  //   text:"Yes, works absolutely!"
+  // });
   // socket.emit("createEmail",{
   //   to:"henryofodile@gmail.com",
   //   text:"Yes lord! I receive with open hands."
   // });
 });
+
+socket.on("newMessage",function(message){
+  console.log("New User available",message)
+})
 socket.on("disconnect",function(){
   console.log("Server has been disconnected!");
 });
 
-socket.on("newEmail", function(email){
-  console.log("You have a new email, please attend to accordingly!",email);
-})
+// socket.on("newEmail", function(email){
+//   console.log("You have a new email, please attend to accordingly!",email);
+// })
--- server/server.js
+++ server/server.js
@@ -5,35 +5,55 @@ var socketIO = require("socket.io");
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
-const server = http.createServer(app);
+const{generateMessage} = require("./utils/message")
+// var socket = io.connect('http://localhost:3000');
+var server = http.createServer(app);
 var io = socketIO(server);
 app.use(express.static(publicPath));
 // var socket = io();
 io.on("connection",(socket)=>{
   console.log("new user connected!");
-});
 
-io.emit("newMessage",{
-  from:"Henry",
-  message:"Hey there,right on point!",
-  completedAt:123123
-})
+//
+socket.emit("newMessage",generateMessage("Admin","Welcome to the chat App"))
+//
+socket.broadcast.emit("newMessage",generateMessage("Admin","New user just joined in!"))
+// io.emit("newMessage",{
+//   from:"Henry",
+//   message:"Hey there,right on point!",
+//   completedAt:123123
+// })
  // io.emit("newEmail",{
  //   from: "God@father.com",
  //   message:"Son Do you know that your worries could be resolved?I would fix it!",
  //   createdAt:123
  // });
 
- io.on("createMessage",(message)=>{
+ socket.on("createMessage",(message)=>{
    console.log("createMessage",message);
- })
+   io.emit("newMessage",generateMessage(message.from,message.text))
+ });
 
- // io.on("createEmail",(newEmail)=>{
+   // {
+   //   from:message.from,
+   //   text:message.text,
+   //   createdAt:new Date.getTime()
+   // })
+
+   // socket.broadcast.emit("newMessage",{
+   //   from:message.from,
+   //   text:message.text,
+   //   createdAt:new Date().getTime()
+   // })
+
+ //
+ // socket.on("createEmail",(newEmail)=>{
  //   console.log("createEmail",newEmail)
- // })
-  io.on("disconnect",()=>{
+ // });
+  socket.on("disconnect",()=>{
     console.log("User was disconnected");
   });
+});
 server.listen(3000, ()=>{
   console.log(`Server, now running up on port ${port}!`)
 });
